82efcaa38031ae74141addd6abecb52ad2c74c9f
iMac <imac@iMac-2.local>
2023-02-23 13:59:58 -0500

    Wrote new program

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+
+        string name = PromptUserName();
+
+        int favoriteNum = PromptUserNumber();
+
+        int squareNum = SquareNumber(favoriteNum);
+
+        DisplayResult(name, squareNum);
     }
+    
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string response = Console.ReadLine();
+        return response;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        string response = Console.ReadLine();
+        int number = int.Parse(response);
+        return number;
+    }
+
+    static int SquareNumber(int num)
+    {
+        int squared = num * num;
+        return squared;
+    }
+
+    static void DisplayResult(string name, int squareNum)
+    {
+        Console.WriteLine($"{name}, the square of you favorite number is {squareNum}");
+    }
+
+
+
+
 }
