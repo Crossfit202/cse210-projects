5b6b25ddb5a9965a6797d66bb26956f266ac0838
iMac <imac@iMac-2.local>
2023-03-03 22:04:42 -0500

    Wrote the main program for my journal assignment

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,53 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Journal journal = new Journal();
+        
+        int input = 0;
+
+        List<string> menu = new List<string>
+        {
+            "\nPlease select one of the following choices:",
+            "1. Write",
+            "2. Display",
+            "3. Save",
+            "4. Load",
+            "5. Quit",
+        };
+
+        while (input != 5)
+        {
+            
+            foreach (string menuItem in menu)
+            {
+                Console.WriteLine(menuItem);
+            }
+            Console.Write("What would you like to do? ");
+            input = int.Parse(Console.ReadLine());
+
+            switch (input)
+            {
+                case 1:
+                    journal.MakeNewEntry();
+                    break;
+
+                case 2:
+                    journal.DisplayEntries();
+                    break;
+                case 3:
+                    journal.SaveToCSV();
+                    break;
+                case 4:
+                    journal.LoadFromCSV();
+                    break;
+                default:
+                    Console.WriteLine("That was not a valid number");
+                    break; 
+            
+
+
+            }
+        }
     }
 }
+
