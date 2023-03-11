153f0a5a0503add84bce48d48ff4e38c179ed77c
iMac <imac@iMac-2.lan>
2023-03-11 11:01:19 -0500

    Rewrote program code

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -7,58 +7,33 @@ class Program
     {   
         Console.Clear();
         Console.WriteLine("Welcome to the Scripture Memorizer Tool!");
-        Console.WriteLine("\nWould you like to insert your own scripture? (Y/N)");
+        Console.WriteLine("\nHow many words would you like to hide each round?");
         string input = Console.ReadLine();
+        int inputNum = int.Parse(input);
 
-        if (input.ToLower() == "n")
-        {
+        
             Reference reference = new Reference("1 Nephi", "1", "3");
-            Scripture scripture = new Scripture(reference, "And I know that the record which I make is true; and I make it with mine own hand; and I make it according to my knowledge.");
-            Word word = new Word(scripture);
+            Scripture scripture = new Scripture(reference, "And I know that the record which I make is true; and I make it with mine own hand; and I make it according to my knowledge.", inputNum);
+            
 
             Console.Clear();
             Console.WriteLine("You will be memorizing 1 Nephi 1:3");
             Console.WriteLine("\nPress enter to continue or type \"quit\" to finish");
             string userInput = Console.ReadLine();
+            scripture.makeList();
+            scripture.displayScripture();
 
-            while (userInput != "quit" && word.IsCompletelyHidden() == true)
+            do
             {
                 Console.Clear();
-                Console.WriteLine(string.Format("{0} {1}", reference.toSingleVerse(), word.toString()));
+                scripture.displayScripture();
                 Console.WriteLine();
                 Console.WriteLine("Press enter to hide words or type \"quit\" to finish");
-                word.hide();
                 userInput = Console.ReadLine();
-
-            }
-        }
-        else
-        {
-            Reference reference = new Reference();
-            Scripture scripture = new Scripture();
+                scripture.hideRandomWords();
+            }while (userInput != "quit" && scripture.IsCompletelyHidden() == false);
     
-            reference.promptReference();
-            scripture.promptScripture();
 
-            Word word = new Word(scripture);
-
-            Console.Clear();
-            Console.WriteLine($"You will be memorizing {reference.toSingleVerse()}");
-            Console.WriteLine("\nPress enter to continue or type \"quit\" to finish");
-            string userInput = Console.ReadLine();
-
-            while (userInput != "quit" && word.IsCompletelyHidden() == true)
-            {
-                Console.Clear();
-                Console.WriteLine(string.Format("{0} {1}", reference.toSingleVerse(), word.toString()));
-                Console.WriteLine();
-                Console.WriteLine("Press enter to hide words or type \"quit\" to finish");
-                word.hide();
-                userInput = Console.ReadLine();
-
-            }
-
-        }
     }
-
 }
+
