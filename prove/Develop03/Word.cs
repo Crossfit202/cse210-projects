7ba452777803f6707d603b5bd17ce50592748dcd
iMac <imac@iMac-2.lan>
2023-03-11 11:02:24 -0500

    rewrote word class

--- prove/Develop03/Word.cs
+++ prove/Develop03/Word.cs
@@ -1,54 +1,43 @@
 class Word
 {
-    private Scripture _scripture;
-    private List<string> _scriptureText;
+    
+  
 
-    public Word(Scripture scripture)
+    private string _word; 
+
+    
+
+    public Word(string word)
     {
-        _scripture = scripture;
-        _scriptureText = new List<string>();
-        makeList();
+        _word = word;
+        
+
     }
 
-    private void makeList()
+    public Word()
     {
-        _scriptureText = _scripture.scriptureString().Split(' ').ToList();
+
+    }
+
+    public string getWord()
+    {
+        return _word.ToString();
     }
 
     public void hide()
     {
-        int totalWordsRemoved = 0;
-        int wordsToHide = new Random().Next(2,4);
-        
-
-        do
-        {
-            int index = new Random().Next(0, _scriptureText.Count());
-            if (_scriptureText[index].Contains('_') == false)
-            {
-                _scriptureText[index] = new string('_', _scriptureText[index].Length);
-                totalWordsRemoved++;
-            }
-        } while (totalWordsRemoved != wordsToHide || IsCompletelyHidden() == false);
+     _word = new string('_', _word.Length);
     }
-
-    public string toString()
-    {
-        return string.Join(' ', _scriptureText);
-    }
-
-    public bool IsCompletelyHidden()
+    
+    public bool isHidden()
     {
         bool value = false;
 
-        foreach (string word in _scriptureText)
+        if (_word.Contains('_'))
         {
-            if (word.Contains('_') == false)
-            {
-                value = true;
-                break;
-            }
+            value = true;
         }
+
         return value;
     }
 }
