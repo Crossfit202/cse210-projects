2fd7721d2926945d44db64d67f06362b7d89e7d9
iMac <imac@iMac-2.lan>
2023-03-13 15:08:21 -0400

    Write main program for assignment project

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment1 = new Assignment("Samuel Bennett", "Multiplication"); 
+
+        Console.Clear();
+        Console.WriteLine(assignment1.GetSumary());
+
+        Console.WriteLine();
+        MathAssignment mathassignment1 = new MathAssignment("7.3", "8-19", "Roberto Rodriguez", "Fractions");
+        Console.WriteLine(mathassignment1.GetHomeworkList());
+        Console.WriteLine();
+        Console.WriteLine(mathassignment1.GetSumary());
+
+        Console.WriteLine();
+        WritingAssignment writingassignment1 = new WritingAssignment("The Causes of Word War II", "Mary Waters", "European History");
+        Console.WriteLine(writingassignment1.GetWritingInformation());
+
+        Console.WriteLine();
+        Console.WriteLine(writingassignment1.GetSumary());
     }
 }
