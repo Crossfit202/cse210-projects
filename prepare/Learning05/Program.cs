5eaa04b0a1d449f88d4245920eee4f319349e50c
iMac <imac@iMac-2.lan>
2023-03-21 16:18:25 -0400

    Wrote the main program for the shapes activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square square = new Square("green", 3);
+        Rectangle rectangle = new Rectangle("blue", 4, 5); 
+        Circle cirlce = new Circle("red", 6);
+
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(cirlce);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"The {shape.GetColor()} shape has an area of {shape.GetArea()}");
+        }
+        
     }
 }
