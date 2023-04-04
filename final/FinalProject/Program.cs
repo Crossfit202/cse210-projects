6ac79e71fa48e72e611192e9dec79a2da58e460d
iMac <imac@iMac-2.lan>
2023-04-04 19:27:58 -0400

    Added 2 players

--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -33,13 +33,14 @@ class Program
                 player.SetName();
                 string playerName = player.GetName();
                 player.SetCharacter(1, false, "none");
-                timer.SetLevel();
+                timer.SetLevel(player.GetCharacter().ToString());
                 int level = timer.GetLevel();
                 CPU computer = new CPU(level);
                 int duration = timer.GetDuration();
                 int newDuration = duration;
                 int hideSuccess = 0; 
                 int findSuccess = 0;
+                int ComHideSuccess = 0;
                 
                 menu.PrintMessage("In this gamemode you will play against the computer. \nYou will use W A S and D to make your choice");
             
@@ -56,8 +57,8 @@ class Program
 
                     if (player.GetCharacter().ToString() == "Runner")
                     {
-                        choice = player.GetCharacter().RoomChoice(playerName, randomRooms);
-                        choice2 = computer.RoomChoice("Computer", randomRooms);
+                        choice = player.GetCharacter().RoomChoice(1, playerName, randomRooms, "player1");
+                        choice2 = computer.RoomChoice(1, "Computer", randomRooms, "computer");
                         
                         selector.RemoveFromList(choice);
                         bool gotCaught = compare.Compare(choice, choice2);
@@ -65,7 +66,7 @@ class Program
                         if(gotCaught)
                         {
                             selector.Lose(player.GetCharacter().ToString());
-                            timer.Spinner(5);
+                            timer.Spinner(3);
                             newDuration = 0;
                         }
                         else if (gotCaught == false)
@@ -80,14 +81,14 @@ class Program
 
                     else if (player.GetCharacter().ToString() == "Chaser")
                     {
-                        choice2 = computer.RoomChoice("Computer", randomRooms);
-                        choice = player.GetCharacter().RoomChoice(playerName, randomRooms);
+                        choice2 = computer.RoomChoice(1, "Computer", randomRooms, "computer");
+                        choice = player.GetCharacter().RoomChoice(1, playerName, randomRooms, "player1");
                         selector.RemoveFromList(choice);
                         bool gotCaught = compare.Compare(choice, choice2);
                         if(gotCaught)
                         {
                             selector.Win(playerName, player.GetCharacter().ToString());
-                            timer.Spinner(5);
+                            timer.Spinner(3);
                             findSuccess += 1;
                             newDuration = 0;
                         }
@@ -95,6 +96,7 @@ class Program
                         {
                             Console.WriteLine("The Runner was not found by the Chaser");
                             timer.Spinner(2);
+                            ComHideSuccess +=1;
                             newDuration = (newDuration - 1); 
                         }                            
                         
@@ -106,110 +108,130 @@ class Program
                 if (hideSuccess == duration)
                 {
                     selector.Win(playerName, "runner");
-                    timer.Spinner(5);
+                    timer.Spinner(3);
                 }
 
                 else if (findSuccess == 1)
                 {
                     selector.Win(playerName, "chaser");
-                    timer.Spinner(5);
+                    timer.Spinner(3);
+                }
+
+                else if (ComHideSuccess == duration)
+                {
+                    selector.Lose("chaser");
                 }
                     
                 break; 
 
                 case 2: // Play against another player
 
-               // CPU computer = new CPU();
-               // Player player = new Player(); 
-               // 
-               // Console.Clear();
-               // player.SetName();
-               // string playerName = player.GetName();
-               // player.SetCharacter(1, false, "none");
-               // timer.SetDuration();
-               // int duration = timer.GetDuration();
-               // int newDuration = duration;
-               // int hideSuccess = 0; 
-               // int findSuccess = 0;
-               // 
-               // menu.PrintMessage("In this gamemode you will play against the computer. \nYou will use W A S and D to make your choice");
-            //
-//
-               // selector.MakeList();
-//
-               // do
-               // {
-               //     List<string> randomRooms = selector.SelectRandomRooms(); //List of 4 random rooms
-               //     string randomRoomOne = randomRooms[0];
-               //     string randomRoomTwo = randomRooms[1];
-               //     string randomRoomThree = randomRooms[2];
-               //     string randomRoomFour = randomRooms[3];
-               //     string choice;
-               //     string choice2;
-               //     
-//
-               //     if (player.GetCharacter().ToString() == "Runner")
-               //     {
-               //         choice = player.GetCharacter().RoomChoice(playerName, randomRooms);
-               //         choice2 = computer.RoomChoice("Computer", randomRooms);
-               //         
-               //         selector.RemoveFromList(choice);
-               //         bool gotCaught = compare.Compare(choice, choice2);
-//
-               //         if(gotCaught)
-               //         {
-               //             selector.Lose(player.GetCharacter().ToString());
-               //             timer.Spinner(5);
-               //             newDuration = 0;
-               //         }
-               //         else if (gotCaught == false)
-               //         {
-               //             Console.WriteLine("The Runner was not found by the Chaser");
-               //             timer.Spinner(2);
-               //             hideSuccess += 1;
-               //             newDuration = (newDuration - 1);
-               //         }
-    //
-               //     }
-//
-               //     else if (player.GetCharacter().ToString() == "Chaser")
-               //     {
-               //         choice2 = computer.RoomChoice("Computer", randomRooms);
-               //         choice = player.GetCharacter().RoomChoice(playerName, randomRooms);
-               //         selector.RemoveFromList(choice);
-               //         bool gotCaught = compare.Compare(choice, choice2);
-               //         if(gotCaught)
-               //         {
-               //             selector.Win(playerName, player.GetCharacter().ToString());
-               //             timer.Spinner(5);
-               //             findSuccess += 1;
-               //             newDuration = 0;
-               //         }
-               //         else if (gotCaught == false)
-               //         {
-               //             Console.WriteLine("The Runner was not found by the Chaser");
-               //             timer.Spinner(2);
-               //             newDuration = (newDuration - 1); 
-               //         }                            
-               //         
-               //     }
-//
-               //     
-               // } while (newDuration != 0);
-//
-               // if (hideSuccess == duration)
-               // {
-               //     selector.Win(playerName, "runner");
-               //     timer.Spinner(5);
-               // }
-//
-               // else if (findSuccess == 1)
-               // {
-               //     selector.Win(playerName, "chaser");
-               //     timer.Spinner(5);
-               // }
+                Player playerOne = new Player(); 
+                Player playerTwo = new Player();
 
+                Console.Clear();
+                Console.WriteLine("Player 1");
+                playerOne.SetName();
+                Console.WriteLine("\nPlayer 2");
+                playerTwo.SetName();
+                string playerOneName = playerOne.GetName();
+                string playerTwoName = playerTwo.GetName();
+                Console.WriteLine("\nPlayer 1:");
+                playerOne.SetCharacter(2, false, "none");
+                playerTwo.SetCharacter(2, true, playerOne.GetCharacter().ToString());
+                timer.SetDuration();
+                int oldDuration = timer.GetDuration();
+                int currentDuration = oldDuration;
+                bool won = false;
+                int hiderSuccess =0;
                 
+                Console.Clear();
+                menu.PrintMessage("In this gamemode you will play against each other. \n Player 1: You will use W A S and D to make your choice. \n Player 2: You will use I J K and L to choose. \nGood luck!");
+                selector.MakeList();
+                
+                do
+                {
+                    List<string> randomRooms = selector.SelectRandomRooms(1); //List of 4 random rooms
+                
+                    string choice;
+                    string choice2;
+
+                        if (playerOne.GetCharacter().ToString() == "Runner")
+                    {
+                        choice = playerOne.GetCharacter().RoomChoice(2, playerOneName, randomRooms, "player1");
+                        choice2 = playerTwo.GetCharacter().RoomChoice(2, playerTwoName, randomRooms, "player2");
+
+                        selector.RemoveFromList(choice);
+                        bool gotCaught = compare.CompareTwoPlayer(choice, choice2);
+                        
+                        if(gotCaught)
+                        {
+                            selector.Win(playerTwoName, "chaser");
+                            timer.Spinner(3);
+                            currentDuration = 0;
+                        }
+
+                        else if (gotCaught == false)
+                        {
+                            Console.WriteLine("The Runner was not found by the Chaser");
+                            timer.Spinner(2);
+                            hiderSuccess += 1;
+                            currentDuration = (currentDuration - 1);
+                            
+                            if (hiderSuccess == oldDuration)
+                            {
+                                won = true;
+                            }    
+                        }
+                    }
+
+                        else if (playerOne.GetCharacter().ToString() == "Chaser")
+                    {
+                        choice2 = playerTwo.GetCharacter().RoomChoice(2, playerTwoName, randomRooms, "player2");
+                        choice = playerOne.GetCharacter().RoomChoice(2, playerOneName, randomRooms, "player1");
+                        selector.RemoveFromList(choice);
+                        bool gotCaught = compare.CompareTwoPlayer(choice, choice2);
+
+                        if(gotCaught)
+                        {
+                            selector.Win(playerOneName, "chaser");
+                            timer.Spinner(3);
+                            currentDuration = 0;
+                        }
+                        else if (gotCaught == false)
+                        {
+                            Console.WriteLine("The Runner was not found by the Chaser");
+                            timer.Spinner(2);
+                            hiderSuccess += 1;
+                            currentDuration = (currentDuration - 1);
+                        
+                            if (hiderSuccess == oldDuration) 
+                            {
+                                won = true;
+                            }
+                        }                            
+
+                    }
+
+                } while (currentDuration != 0);
+                
+                
+                if (won == true)
+                {
+                    if (playerOne.GetCharacter().ToString().ToLower() == "runner")
+                    {
+                        selector.Win(playerOneName, "runner");
+                        timer.Spinner(3);   
+                    }
+
+                    else 
+                    {
+                        selector.Win(playerTwoName, "runner");
+                        timer.Spinner(3);
+                    }
+                }
+                
+            
                 break;
             }
         }
